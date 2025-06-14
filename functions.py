b41dc7f40f396975048d127cdc1184d03440f18b
Zelda Rose <ZRose1@uco.edu>
2025-06-14 11:15:44 -0500

    First file added

---
+++ functions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File contains functions to be used in the main program.
+The main program will be driven by a GUI programmed in TKinter if I do not
+find an easier library to work with.
+
+Created on Sat Jun 14 11:09:11 2025
+
+@author: zelda
+"""
+
+
+if __name__ == "__main__":
+    pass
