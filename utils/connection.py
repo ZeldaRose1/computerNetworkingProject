8298bf3bf1190daa1092444acbb143a3cf33a532
Zelda Rose <ZRose1@uco.edu>
2025-07-03 09:34:33 -0500

    Updated send and listen functions in the Connection class.

--- utils/connection.py
+++ utils/connection.py
@@ -9,6 +9,8 @@ Created on Thu Jun 19 08:14:37 2025
 from socket import socket
 from socket import AF_INET
 from socket import SOCK_STREAM
+from socket import SOL_SOCKET
+from socket import SO_REUSEADDR
 
 
 from utils.config import Config
@@ -25,10 +27,15 @@ class Connection:
     #     self.con_in = socket(family=AF_INET, type=SOCK_STREAM)
     #     self.con_in.bind(("0.0.0.0", conf.default_port))
 
-    def listen(self):
+    def listen(self) -> None:
         """Listen for incoming connections"""
-        self.listen_stop()
+        # Ensure ports are clear in case of multiple calls
+        # self.listen_stop()
+        # Build initial socket connection for TCP communication
         self.con_in = socket(family=AF_INET, type=SOCK_STREAM)
+        # Enable a safe way to reuse sockets in case they still have latent
+        # traffic
+        self.con_in.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.con_in.bind(("0.0.0.0", conf.default_port))
         self.con_in.listen()
         self.sender_ip, self.in_port = self.con_in.accept()
@@ -37,33 +44,78 @@ class Connection:
         # manually closed.
         while True:
             data = self.sender_ip.recv(1024)
+            if not data:
+                print("[Server] Client closed conn. Ending server conn")
+                self.con_in.close()
+                break
             print(data.decode())
-            self.sender_ip.sendall("Hello From the same Side!".encode())
-            self.sender_ip.close()
-            break
+            self.sender_ip.sendall("Hello From the server Side!".encode())
         self.con_in.close()
 
-    def listen_stop(self):
+    def listen_stop(self) -> None:
         """Free inbound port and friend connection if it exists"""
-        if "con_in" in dir(self):
+        if hasattr(self, "con_in"):
             self.con_in.close()
             del self.con_in
-        if "sender_ip" in dir(self):
+        if hasattr(self, "sender_ip"):
             self.sender_ip.close()
             del self.sender_ip
             del self.in_port
 
     # Start functions.
-    def send(self, ip: str, port: int) -> None:
+    def open_outbound_connection(self, ip: str, port: int) -> None:
+        """Open and retain a connection for outbound connections"""
+        self.con_out = socket(family=AF_INET, type=SOCK_STREAM)
+        self.con_out.connect((ip, port))
+        print("Connection Opened")
+
+    def send(self, dat_out: bytes) -> None:
         # TODO: recode this so it also includes a data object argument
         """Send connection request to address and socket from friend"""
-        with socket(family=AF_INET, type=SOCK_STREAM) as s:
-            s.connect((ip, port))
-            s.send("Hello from the other side!".encode())
-            data = s.recv(1024)
+        if not hasattr(self, "con_out"):
+            print("Error: Must open connection before sending data.")
+        else:
+            self.con_out.send(dat_out.encode())
+            dat_in = self.con_out.recv(1024)
 
-        if data:
-            print(data.decode())
+        if dat_in:
+            print(dat_in.decode())
+
+    def initiate_hole_punch(
+            self,
+            dstip: str,
+            dstpt: int,
+            srcpt: int = conf.default_port
+    ) -> None:
+        """
+        Function to initiate hole punching
+
+        Params:
+            dstip: IP address to initiate connection with
+            dstpt: Port to send data to
+            srcpt: Port to send data from
+
+        Returns:
+            None
+        """
+        # Create socket
+        self.con_in = socket(family=AF_INET, type=SOCK_STREAM)
+        # Set socket to re-use connection to prevent in-use errors
+        self.con_in.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.con_in.bind(("0.0.0.0", conf.default_port))
+        self.con_in.setblocking(False)
+
+        # Send initial packet to punch hole.
+        try:
+            self.con_in.send((dstip, dstpt))
+        except Exception as e:
+            print(str(e))
+            pass  # Error thrown since connection necessarily fails.
+
+        # Listen for a given time until connected or timeout
+        while True:
+            dat = self.con_in.recv(1024)
+
 
     def __del__(self):
         """Destructor; Close connections and clear ports"""
